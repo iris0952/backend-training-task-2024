fa3fb3ad3d51fc09c424136250cfc9f807fa4d69
Cami Wang <iris0952@gmail.com>
2025-01-07 19:27:03 +0800

    update

--- week1/index.js
+++ week1/index.js
@@ -2,12 +2,16 @@
 // 情境：Alex 正在健身房鍛鍊，他正在使用跑步機慢跑，請用變數描述以下狀態：
 
 // 1. 請宣告一個 `const` 變數名稱為 `alexAge`，並賦予值為 `25`。
-// 2. 請宣告一個 `const` 變數名稱為 `alexMembershipID`，並賦予值為字串 `"GYM2024-12345"`。
-// 3. 請宣告一個 `const` 變數名稱為 `isRunningOnTreadmill`，並賦予值為 `true`。
-// 4. 使用 `console.log()` 輸出 `alexAge`、`alexMembershipID` 和 `isRunningOnTreadmill`。
-
-// 1.1 範例
 const alexAge = 25;
+// 2. 請宣告一個 `const` 變數名稱為 `alexMembershipID`，並賦予值為字串 `"GYM2024-12345"`。
+const alexMembershipID = "GYM2024-12345";
+// 3. 請宣告一個 `const` 變數名稱為 `isRunningOnTreadmill`，並賦予值為 `true`。
+const isRunningOnTreadmill = true;
+// 4. 使用 `console.log()` 輸出 `alexAge`、`alexMembershipID` 和 `isRunningOnTreadmill`。
+console.log(alexAge, alexMembershipID, isRunningOnTreadmill);
+
+
+
 
 // ### 題目二：變數命名練習
 // - 瑜伽團課 - 300 元
@@ -16,28 +20,38 @@ const alexAge = 25;
 // 情境：Alex 這個月的運動預算有 3000 元
 // 請修改以下中文變數名稱，讓他符合變數語意
 
-const 變數一 = 300;
-const 變數二 = 500;
-const 變數三 = 1500;
-const 變數四 = 3000;
+const yogaGroupClass = 300;
+const reTrainingGroupClass = 500;
+const reTrainingOneByOneLessons = 1500;
+const campaignBudget = 3000;
 
 // ### 題目三：變數計算
 // 呈上題，Alex 想要規劃好自己的運動課程，需符合以下三個條件，請將花費總數計算在 AlexBudget 上，一起幫幫他吧！
 // 條件一：一定至少要買 1 堂重訓團課和瑜伽團課
 // 條件二：瑜伽團課只能一次買 3 堂
 // 條件三：一定要花到 2400 以上，並購買 5 堂課程
-
+let AlexBudget = campaignBudget - (yogaGroupClass*3 + reTrainingGroupClass + reTrainingOneByOneLessons);
 console.log(`Alex 買完課程了，他一共剩下 ${AlexBudget} 元`);
+
 
 // ### 題目四：線稿圖截圖，看圖宣告變數
 // 請參考資料夾內 q4.webp 圖片
 // 請依照你看到的內容來嘗試設計變數和值（至少 3 個）
+const lessons = 14;
+const lessonTime = '50min';
+const averageLessonPrice = 180;
+const totalPrice = lessons * averageLessonPrice;
 
 // ### 題目五：布林值與變數定義，看是否有用對 const, let
 // 情境：Alex 在往健身房的路上，望向城市的風景，請描述她看到的一切，並宣告變數與賦予值
 // 4-1. Alex 在等紅綠燈，他抬頭看一下現在是紅燈，還有 28 秒綠燈（最多 3 個宣告）
+let redLight = true;
+let greenLight = 28;
 // 4-2. 目前一起等待的機車有 8 台
+let motocycle = 8;
 // 4.3. Alex 望向天空，看到天上有 5 朵白雲和 1 顆太陽
+let whiteCloud = 5;
+const sun = 1;
 
 // ### 題目六：情境題：簡單變數計算
 // 情境：Alex 每天都會帶著 2000cc 的水壺
@@ -50,6 +64,9 @@ console.log(`Alex 買完課程了，他一共剩下 ${AlexBudget} 元`);
 
 let myWater = 2000; // 水壺容量
 myWater -= 500; // 早上喝了 500cc
+myWater -= 800; // 中午喝了 800cc
+myWater += 1000; // 去健身前裝了 1000cc 的水
+myWater -= 700; // 健身時喝掉了 700cc
 
 console.log(`Alex 的水壺還有 ${myWater}cc 的水`);
 
@@ -62,6 +79,13 @@ console.log(`Alex 的水壺還有 ${myWater}cc 的水`);
 
 let totalBill = 0;
 const machineUsePrice = 50;
+const groupClassesPrice = 150;
+const machineUseTime = 3;
+const groupClasses = 2;
+
+let machineUsePriceTotal = machineUseTime * machineUsePrice;
+let groupClassesTotal = groupClasses * groupClassesPrice;
+let total = machineUsePriceTotal + groupClassesTotal;
 
 console.log(
   `Anna 本週器械使用費共 ${machineUsePriceTotal} 元，團體課費用共 ${groupClassesTotal} 元，一共消費金額是 ${total}元`
@@ -71,31 +95,35 @@ console.log(
 // 情境：請依照以下程式碼告知答案是多少，並在下方用註解方式寫上這五行程式碼做了什麼事
 // 以下程式碼請勿變更
 let a = 8; // 範例：宣告了一個 a 的變數，並賦予了一個 8 的數字型別
-let b = 0;
-a = 13;
-a = b + 4;
-a - b;
-b += 1;
+let b = 0; // 宣告了一個 b 的變數，並賦予了一個 0 的數字型別
+a = 13; // 將 a 的變數重新賦予了一個 13 的數字型別
+a = b + 4; // 將 a 的變數重新賦予了一個 b 的變數加上 4 的數字型別
+a - b; // 將 a 的變數重新賦予了一個 a 的變數減去 b 的變數
+b += 1; // 將 b 的變數重新賦予了一個 b 的變數加上 1 的數字型別
 
 // ### 題目九：型別查詢
 // 請不要觀看 console.log，透過註解告知解答每個變數的型別
-let c = 'world';
-let d = 456;
-let e = c + d;
-let f = false;
-let g = d + d;
+let c = 'world'; 
+let d = 456; 
+let e = c + d; 
+let f = false; 
+let g = d + d; 
 let h = f + g;
 
 // 請從以下新增註解，告知上面每行各別是哪些型別
-// a 是 string
-// b 是 ???
+// c 的型別是 string
+// d 的型別是 number
+// e 的型別是 string
+// f 的型別是 boolean
+// g 的型別是 number
+// h 的型別是 number
 
 // ### 題目十：傳值與傳參考
 // 情境：請依照程式碼告知答案是多少，並在下方用註解方式寫上這五行程式碼做了什麼事
 // 以下程式碼請勿變更
 
-let numberArr1 = [5, 10, 15];
-let numberArr2 = numberArr1;
-numberArr2.push(20);
-numberArr2 = [25, 30, 35];
-console.log(numberArr1, numberArr2);
+let numberArr1 = [5, 10, 15];// 宣告了一個 numberArr1 的變數，並賦予了一個 [5, 10, 15] 的陣列
+let numberArr2 = numberArr1;// 宣告了一個 numberArr2 的變數，並賦予了 numberArr1 的變數
+numberArr2.push(20); // 將 numberArr2 的變數重新賦予了一個 numberArr2 的變數加上 20 的數字型別
+numberArr2 = [25, 30, 35];// 將 numberArr2 的變數重新賦予了一個 [25, 30, 35] 的陣列
+console.log(numberArr1, numberArr2);// Ans: [5, 10, 15, 20] [25, 30, 35]
