31d10b5a2139f4c5b27c12713aa57b0be3ac7a4d
Cami Wang <iris0952@gmail.com>
2025-02-18 20:15:16 +0800

    week3

--- week3/index.js
+++ week3/index.js
@@ -1,17 +1,39 @@
 // 題目 - 樂呵呵健身房
 // 樂呵呵健身房目前有 8 位會員
 const members = ["Alice", "Bob", "Charlie", "Diana", "Evan", "Fiona", "George", "Hannah"];
-
+let purchaseRecords =[];
 // 第一階段：新增課程購買記錄（優惠定價）
 // 撰寫函式 addPurchaseRecord，用於新增會員購買課程的記錄，並依購買數量套用優惠價格：
 let purchaseCourseNum = 0;
-function addPurchaseRecord() {
-    if (condition) {
-        
-    } else if (){
-        
+let unitPrice = 0;
+let totalPrice = 0;
+function addPurchaseRecord(name, courses) {
+    if((name === "") || (name === undefined) || (typeof courses != "number") || (courses <= 0)){
+        console.log(" 輸入錯誤，請輸入有效的會員名稱和課程數量。\n");
+        return;
     }
+    if(courses <= 10){
+        unitPrice = 1500;
+    }else if (courses <= 20) {
+        unitPrice = 1300;
+    }else if (courses > 20) {
+        unitPrice = 1100;
+    }
+    totalPrice = unitPrice * courses;
+    purchaseRecords.push({
+        name,
+        courses,
+        unitPrice,
+        totalPrice
+    })
+    console.log(`新增購買記錄成功！會員 ${name} 購買 ${courses} 堂課，總金額為 ${totalPrice} 元。`);
+    
 }
+ddPurchaseRecord("Alice", 4);
+addPurchaseRecord("Bob", 12); 
+addPurchaseRecord("Charlie", 25); 
+addPurchaseRecord("Hannah", 50); 
+addPurchaseRecord("名稱", "課程數量"); 
 // 購買數量 / 單價 (每堂課)
 
 // 1-10 堂 / 1500 元
@@ -33,15 +55,30 @@ function addPurchaseRecord() {
 // addPurchaseRecord("Bob", 12); >> 印出 console.log 文字為 新增購買記錄成功！會員 Bob 購買 12 堂課，總金額為 15600 元。
 // addPurchaseRecord("Charlie", 25); >> 印出 console.log 文字為 新增購買記錄成功！會員 Charlie 購買 25 堂課，總金額為 27500 元。
 // addPurchaseRecord("Hannah", 50); >> 印出 console.log 文字為 新增購買記錄成功！會員 Hannah 購買 50 堂課，總金額為 55000 元。
-// addPurchaseRecord("名稱", “課程數量”); >> 印出 console.log 文字為 輸入錯誤，請輸入有效的會員名稱和課程數量。
+// addPurchaseRecord("名稱", "課程數量"); >> 印出 console.log 文字為 輸入錯誤，請輸入有效的會員名稱和課程數量。
 
 
 // 第二階段：計算目前的總營業額
 // 新增函式 calculateTotalPrice，計算目前的總營業額為多少。
-
+function calculateTotalPrice() {
+    purchaseRecords.forEach((item) => {
+        totalPrice += item.totalPrice;
+    })
+    console.log(`目前總營業額為 ${totalPrice} 元\n`);
+}
 // 印出 console.log 文字為 目前總營業額為 ${totalPrice} 元
 
 // 第三階段：篩選出還沒有購課的會員
 // 新增函式 filterNoPurchaseMember，篩選特定條件的會員記錄。例如：未購買過課程的會員，並依序列出
-
+let buyCourseMember = [];
+function filterNoPurchaseMember() {
+    purchaseRecords.forEach((item) => {
+        buyCourseMember.push(item.name);
+    })
+    let neverBuyMember = members.filter((member) => {
+        return buyCourseMember.includes(member) === false;
+    })
+    console.log(`未購買課程的會員有：${neverBuyMember}\n`);
+}
 // 印出 console.log 文字為 未購買課程的會員有：.......
+filterNoPurchaseMember();
